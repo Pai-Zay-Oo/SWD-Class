39c9e590c6a929d60ea306af1161f840892b8701
Pai-Zay-Oo <paizayoo.div22@gmail.com>
2022-05-20 16:22:28 +0630

    update

---
+++ js.lessons/news.js
@@ -0,0 +1,266 @@
+ let data = {
+    "status": "ok",
+    "totalResults": 22624,
+    "articles": [
+    {
+    "source": {
+    "id": null,
+    "name": "Frandroid"
+    },
+    "author": "Marie Lizak",
+    "title": "Il ne faudra pas compter que sur la voiture électrique contre le réchauffement climatique",
+    "description": "Selon un rapport de l'ONG InfluenceMap, le nombre de voitures électriques actuellement en circulation serait insuffisant pour limiter le réchauffement climatique. Cela fait quelques années déjà que les pouvoirs publics font tout pour inciter les automobiliste…",
+    "url": "https://www.frandroid.com/produits-android/automobile/voitures-electriques/1340793_il-ne-faudra-pas-compter-que-sur-la-voiture-electrique-contre-le-rechauffement-climatique",
+    "urlToImage": "https://images.frandroid.com/wp-content/uploads/2021/04/voiture-electrique.jpg",
+    "publishedAt": "2022-05-20T09:02:43Z",
+    "content": "Selon un rapport de l'ONG InfluenceMap, le nombre de voitures électriques actuellement en circulation serait insuffisant pour limiter le réchauffement climatique. \r\nUne voiture électrique // Source :… [+2901 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Les Numériques"
+    },
+    "author": "Erick Fontaine",
+    "title": "Dossier : Premier contact avec la Volkswagen ID.5 : un concentré de nouvelles technologies récompensées par Euro NCAP",
+    "description": "Après Audi et Skoda, Volkswagen propose enfin la déclinaison de son SUV coupé électrique ID.5. Nous avons pu le prendre en main rapidement, ainsi que sa version ID.5 GTX. Ce SUV coupé fait la part belle aux aides à la conduite.",
+    "url": "https://www.lesnumeriques.com/voiture/premier-contact-avec-la-volkswagen-id-5-un-concentre-de-nouvelles-technologies-recompensees-par-euro-ncap-a183257.html",
+    "urlToImage": "https://cdn.lesnumeriques.com/optim/article/18/183257/72e1655f-premier-contact-avec-la-volkswagen-id-5-un-concentre-de-nouvelles-technologies-recompensees-par-euro-ncap__1200_630__0-204-908-680.jpeg",
+    "publishedAt": "2022-05-20T09:00:00Z",
+    "content": "Après Audi et Skoda, Volkswagen propose enfin la déclinaison de son SUV coupé électrique ID.5. Nous avons pu le prendre en main rapidement, ainsi que sa version ID.5 GTX. Ce SUV coupé fait la part be… [+13029 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "heise online"
+    },
+    "author": "Dennis Schirrmacher",
+    "title": "Hacking-Wettbewerb Pwn2Own: Teams, Ubuntu und Windows 11 sind bereits gefallen",
+    "description": "Sicherheitsforscher treffen sich in Vancouver, hacken sich durch populäre Anwendungen und Betriebssysteme – und streichen Geldprämien ein.",
+    "url": "https://www.heise.de/news/Hacking-Wettbewerb-Pwn2Own-Teams-Ubuntu-und-Windows-11-sind-bereits-gefallen-7101169.html",
+    "urlToImage": "https://heise.cloudimg.io/bound/1200x1200/q85.png-lossy-85.webp-lossy-85.foil1/_www-heise-de_/imgs/18/3/5/4/7/6/5/6/TeamOrca-38185cd19e93bc7d.jpg",
+    "publishedAt": "2022-05-20T08:53:00Z",
+    "content": "Beim Hacking-Wettbewerb Pwn2Own versuchen Sicherheitsforscher in einem festgesteckten Zeitrahmen Software über bislang unbekannte Sicherheitslücken (Zero Day) zu attackieren. Bei der aktuellen Ausgab… [+1996 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "heise online"
+    },
+    "author": "Dennis Schirrmacher",
+    "title": "Hacking-Wettbewerb Pwn2Own: Teams, Ubuntu und Windows 11 sind bereits gefallen",
+    "description": "Sicherheitsforscher treffen sich in Vancouver, hacken sich durch populäre Anwendungen und Betriebssysteme – und streichen Geldprämien ein.",
+    "url": "https://www.heise.de/news/Hacking-Wettbewerb-Pwn2Own-Teams-Ubuntu-und-Windows-11-sind-bereits-gefallen-7101169.html",
+    "urlToImage": "https://heise.cloudimg.io/bound/1200x1200/q85.png-lossy-85.webp-lossy-85.foil1/_www-heise-de_/imgs/18/3/5/4/7/6/5/6/TeamOrca-38185cd19e93bc7d.jpg",
+    "publishedAt": "2022-05-20T08:53:00Z",
+    "content": "Beim Hacking-Wettbewerb Pwn2Own versuchen Sicherheitsforscher in einem festgesteckten Zeitrahmen Software über bislang unbekannte Sicherheitslücken (Zero Day) zu attackieren. Bei der aktuellen Ausgab… [+1996 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "N-tv.de"
+    },
+    "author": "n-tv NACHRICHTEN",
+    "title": "\"Völlig unwahr\": Elon Musk weist Belästigungsvorwürfe zurück",
+    "description": "Schwere Vorwürfe gegen Elon Musk: Der Milliardär soll 2016 eine Flugbegleiterin sexuell belästigt haben. Um eine Klage beizulegen, soll seine Firma SpaceX der Frau 250.000 Dollar gezahlt haben. Musk weist jedoch alle Anschuldigungen zurück und vermutet eine p…",
+    "url": "https://www.n-tv.de/wirtschaft/Elon-Musk-weist-Belaestigungsvorwuerfe-zurueck-article23345558.html",
+    "urlToImage": "https://bilder4.n-tv.de/img/incoming/crop23345599/554132251-cImg_16_9-w1200/286769186.jpg",
+    "publishedAt": "2022-05-20T08:52:18Z",
+    "content": "Schwere Vorwürfe gegen Elon Musk: Der Milliardär soll 2016 eine Flugbegleiterin sexuell belästigt haben. Um eine Klage beizulegen, soll seine Firma SpaceX der Frau 250.000 Dollar gezahlt haben. Musk … [+2519 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Hibridosyelectricos.com"
+    },
+    "author": "Javier Gómara",
+    "title": "Polestar recorta en 15.000 unidades sus previsiones de ventas para este 2022",
+    "description": "La situación epidemiológica en China está complicando cumplir con los objetivos de fabricación. Polestar se ha visto obligada a bajar sus expectativas para el curso.",
+    "url": "https://www.hibridosyelectricos.com/articulo/actualidad/polestar-recorta-15000-unidades-previsiones-ventas-2022/20220520104716058160.html",
+    "urlToImage": "https://www.hibridosyelectricos.com/media/hibridos/images/2022/05/20/2022052010433736081.jpg",
+    "publishedAt": "2022-05-20T08:47:16Z",
+    "content": "Está pasando en todas las marcas. Cuando todo el mundo esperaba que el 2022 se convirtiese en un año de recuperación, múltiples dificultades están afectando a la industria del automóvil. Casi todas l… [+2744 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Lupa.cz"
+    },
+    "author": "redakce@lupa.cz (Lupa.cz: Jan Sedlák)",
+    "title": "Slovensko chce postavit nejrychlejší AI superpočítač na světě, použije vlastní čipy",
+    "description": "Slovenská společnost Tachyum po oznámení univerzálního serverového procesoru Prodigy se 128 jádry přichází s dalším velkolepým prohlášením. Na Slovensku chce rozjet superpočítač za 70 milionů eur, tedy 1,7 miliardy korun, který má ve zpracování AI operací zvl…",
+    "url": "https://www.lupa.cz/aktuality/slovensko-chce-postavit-nejrychlejsi-ai-superpocitac-na-svete-pouzije-vlastni-cipy/",
+    "urlToImage": "https://i.iinfo.cz/images/563/tachyum-prodigy.jpg",
+    "publishedAt": "2022-05-20T08:47:16Z",
+    "content": "Slovenská spolenost Tachyum po oznámení univerzálního serverového procesoru Prodigy se 128 jádry pichází s dalím velkolepým prohláením. Na Slovensku chce rozjet superpoíta za 70 milion eur, tedy 1,7 … [+4645 chars]"
+    },
+    {
+    "source": {
+    "id": "der-tagesspiegel",
+    "name": "Der Tagesspiegel"
+    },
+    "author": null,
+    "title": "Musk weist Vorwürfe sexueller Belästigung zurück",
+    "description": "Nach einem politischen Tweet erwartete der Tesla-Chef eine „Kampagne schmutziger Tricks“. Schon einen Tag später muss er sich schwerer Vorwürfe erwehren.",
+    "url": "https://www.tagesspiegel.de/gesellschaft/panorama/tesla-chef-verlangt-beweise-von-luegnerin-musk-weist-vorwuerfe-sexueller-belaestigung-zurueck/28361488.html",
+    "urlToImage": "https://www.tagesspiegel.de/images/files-in-this-file-photo-taken-on-february-10-2022-elon-musk-gestures-as-he-speaks-during-a-press-conference-at-spacexs-starbase-facility-near-boca-chica-village-in-south-texas-tesla-boss-elon-musks-road-to-turning-twitter-into-a-money-making-platform-where-anyone-can-say-anything-looks-to-e/28361702/1-format530.jpg",
+    "publishedAt": "2022-05-20T08:47:06Z",
+    "content": "Tech-Milliardär Elon Musk hat einen Vorwurf sexueller Belästigung gegen ihn zurückgewiesen. Und, für die Akten, diese wilden Anschuldigungen sind absolut unwahr, schrieb Musk in der Nacht zum Freitag… [+3603 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Tomshw.it"
+    },
+    "author": "Marco Doria",
+    "title": "Windows 11 e Microsoft Teams violati al day one del Pwn2Own 2022",
+    "description": "Pwn2Own è una competizione tra hacker che ha lo scopo di individuare vulnerabilità e exploit, con ricchi premi in denaro e prodotti hardware.",
+    "url": "https://www.tomshw.it/hardware/windows-11-e-microsoft-teams-violati-al-day-one-del-pwn2own-2022/",
+    "urlToImage": "https://www.tomshw.it/images/images/2022/05/pwn2own-2022-230564.jpg",
+    "publishedAt": "2022-05-20T08:46:22Z",
+    "content": "Software Windows 11 e Microsoft Teams violati al day one del Pwn2Own 2022\r\nTom's Hardware vive grazie al suo pubblico. Quando compri qualcosa dai nostri link, potremmo guadagnare una commissione. Sco… [+2978 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Presse-citron"
+    },
+    "author": "Romain Pomian-Bonnemaison",
+    "title": "SpaceX : Elon Musk aurait harcelé sexuellement une employée",
+    "description": "Elon Musk se serait livré à du harcèlement sexuel envers une employée. SpaceX aurait ensuite signé un gros chèque pour étouffer l'affaire - sur Twitter, l'intéressé parle d'attaque politiquement motivée.",
+    "url": "https://www.presse-citron.net/spacex-elon-musk-aurait-harcele-sexuellement-une-employee/",
+    "urlToImage": "https://www.presse-citron.net/app/uploads/2021/09/spacex-starship.jpg",
+    "publishedAt": "2022-05-20T08:43:32Z",
+    "content": "L’affaire fait du bruit depuis 24 heures. Le site Business Insider révèle que SpaceX aurait couvert une affaire de harcèlement sexuel impliquant Elon Musk. L’employée serait une hôtesse de l’air empl… [+3098 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Les Numériques"
+    },
+    "author": "Erick Fontaine",
+    "title": "Actualité : Ionity fête sa 100e station de charge en France et propose une tarification au kWh",
+    "description": "Le réseau européen de bornes de recharge rapide Ionity fête la mise en service de sa 100e station en France. Le consortium propose désormais une tarification au kWh en lieu et place de la facturation à la minute.",
+    "url": "https://www.lesnumeriques.com/voiture/ionity-fete-sa-100e-station-de-charge-en-france-et-propose-une-tarification-au-kwh-n183409.html",
+    "urlToImage": "https://cdn.lesnumeriques.com/optim/news/18/183409/562ca258-ionity-fete-sa-100eme-station-de-charge-en-france-et-propose-la-tarification-au-kwh__1200_630__0-112-1500-899.jpeg",
+    "publishedAt": "2022-05-20T08:41:00Z",
+    "content": "Le réseau européen de bornes de recharge rapide Ionity fête la mise en service de sa 100e station en France. Le consortium propose désormais une tarification au kWh en lieu et place de la facturation… [+3331 chars]"
+    },
+    {
+    "source": {
+    "id": "handelsblatt",
+    "name": "Handelsblatt"
+    },
+    "author": "Handelsblatt",
+    "title": "SpaceX: Elon Musk weist Vorwürfe sexueller Belästigung nach Medienbericht zurück",
+    "description": "Dem Tesla-Chef wird vorgeworfen, sich vor einer Flugbegleiterin in seinem Privatjet entblößt zu haben. Musk wittert er eine politische Kampagne gegen sich.",
+    "url": "https://www.handelsblatt.com/technik/forschung-innovation/spacex-elon-musk-weist-vorwuerfe-sexueller-belaestigung-nach-medienbericht-zurueck/28361452.html",
+    "urlToImage": "https://www.handelsblatt.com/images/elon-musk-/28361588/3-format2003.jpg",
+    "publishedAt": "2022-05-20T08:40:00Z",
+    "content": "Oakland Tesla-Chef Elon Musk hat Vorwürfe sexueller Belästigung als völlig unwahr zurückgewiesen. Das Nachrichtenportal Business Insider hatte am Donnerstag berichtet, der Milliardär habe 2016 eine F… [+2630 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Hibridosyelectricos.com"
+    },
+    "author": "Javier Gómara",
+    "title": "Algunos clientes del Tesla Model X llevan esperando su coche 19 meses, y lo que les queda",
+    "description": "Los clientes de Tesla suelen tener más paciencia de lo habitual. Los plazos de entrega están cambiando en función del mercado. El Tesla Model X acumula meses de retraso, y cada vez suma más.",
+    "url": "https://www.hibridosyelectricos.com/articulo/actualidad/plazos-entrega-tesla-model-x-19-meses/20220520103542058157.html",
+    "urlToImage": "https://www.hibridosyelectricos.com/media/hibridos/images/2022/05/20/2022052010291122471.jpg",
+    "publishedAt": "2022-05-20T08:35:42Z",
+    "content": "La paciencia tiene un límite, y muchos conductores están probando los límites de su paciencia a la hora de recibir su coche nuevo. A muchos compradores del Tesla Model X la espera se les está haciend… [+2909 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Shiftdelete.net"
+    },
+    "author": "Tunahan Karakış",
+    "title": "Elon Musk için şoke eden cinsel taciz iddiası: İlk açıklama geldi!",
+    "description": "Tesla ve SpaceX CEO'su Elon Musk, ağır bir suçlamayla karşı karşıya. İddialara göre iş insanı, bir SpaceX çalışanını taciz etti.\nBu içerik ilk olarak Elon Musk için şoke eden cinsel taciz iddiası: İlk açıklama geldi! adresinde yayınlandı Teknoloji Haberleri -…",
+    "url": "https://shiftdelete.net/elon-musk-icin-soke-eden-cinsel-taciz-iddiasi-ilk-aciklama-geldi",
+    "urlToImage": "https://i0.wp.com/shiftdelete.net/wp-content/uploads/2022/01/elonmuskuzgun.jpg?fit=1280%2C720&ssl=1",
+    "publishedAt": "2022-05-20T08:30:00Z",
+    "content": "Son zamanlarda sosyal medya platformu Twitter nedeniyle gündemden dümeyen Tesla ve SpaceX CEO’su Elon Musk, ar bir suçlamayla kar karya. ddialara göre Musk, 2016 ylnda bir SpaceX çalanna cinsel taciz… [+2079 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Feber.se"
+    },
+    "author": "Wille Wilhelmsson",
+    "title": "Elon Musk anklagas för sexuella trakasserier",
+    "description": "Sägs ha erbjudit häst som betalning för sex\n\n\n\n\n\n\nSpaceX och Teslas vd Elon Musk anklagas nu för sexuella trakasserier mot en kvinna som arbetade som flygvärdinna och massör ombord på ett av SpaceX flygplan.\n\nDet hela ska ha skett 2016 då Musk under en massag…",
+    "url": "https://feber.se/samhalle/elon-musk-anklagas-for-sexuella-trakasserier/438524/",
+    "urlToImage": "https://static.feber.se/article_images/53/69/53/536953.jpeg",
+    "publishedAt": "2022-05-20T08:30:00Z",
+    "content": "SpaceX och Teslas vd Elon Musk anklagas nu för sexuella trakasserier mot en kvinna som arbetade som flygvärdinna och massör ombord på ett av SpaceX flygplan.\r\nDet hela ska ha skett 2016 då Musk under… [+1158 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Vnexpress.net"
+    },
+    "author": "VnExpress",
+    "title": "Tesla muốn nhân viên ở Trung Quốc làm việc trong 'bong bóng'",
+    "description": "Dù Thượng Hải đang nới lỏng các biện pháp giãn cách, Tesla vẫn lên kế hoạch cho công nhân ở trong \"bong bóng khép kín\" đến giữa tháng 6.",
+    "url": "https://vnexpress.net/tesla-muon-nhan-vien-o-trung-quoc-lam-viec-trong-bong-bong-4465910.html",
+    "urlToImage": "https://vcdn1-kinhdoanh.vnecdn.net/2022/05/20/800x1-1653022247-8522-1653022491.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=2HXxMrDbO77J9eqpClvb4A",
+    "publishedAt": "2022-05-20T08:26:33Z",
+    "content": "Dù Thng Hi ang ni lng các bin pháp giãn cách, Tesla vn lên k hoch cho công nhân trong \"bong bóng khép kín\" n gia tháng 6.Hàng nghìn công nhân ca Tesla ti nhà máy Thng Hi ang phi ng trên sàn. H làm vi… [+1454 chars]"
+    },
+    {
+    "source": {
+    "id": "independent",
+    "name": "Independent"
+    },
+    "author": "Holly Bancroft",
+    "title": "Elon Musk denies claims he exposed himself to flight attendant and offered to buy her a horse",
+    "description": "Claims of sexual harassment are ‘utterly untrue’ and ‘politically motivated’, tweets Tesla chief executive",
+    "url": "https://www.independent.co.uk/news/world/americas/elon-musk-twitter-sexual-harassment-claims-tesla-b2083448.html",
+    "urlToImage": "https://static.independent.co.uk/2022/05/20/08/SEI105058632.jpg?quality=75&width=1200&auto=webp",
+    "publishedAt": "2022-05-20T08:25:08Z",
+    "content": "Billionaire Elon Musk has denied he sexually harassed a flight attendant following reports that he exposed himself on a 2016 private jet flight to London. \r\nThe Tesla chief executive rejected wild ac… [+2672 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Numerama"
+    },
+    "author": "Raphaëlle Baut",
+    "title": "Fin de la tarification à la minute chez Ionity : un changement positif (mais pas pour tout le monde)",
+    "description": "À partir du 1er juillet 2022, Ionity va changer ses tarifs pour facturer les clients au kilowattheure en France. C’est la fin d’une spécificité bien française, qui favorisait certains véhicules électriques aux dépens des autres. C’est à l’occasion de l’inaugu…",
+    "url": "https://www.numerama.com/vroom/970181-fin-de-la-tarification-a-la-minute-chez-ionity-un-changement-positif-mais-pas-pour-tout-le-monde.html",
+    "urlToImage": "https://www.numerama.com/wp-content/uploads/2021/11/kia-ev6-7.jpg",
+    "publishedAt": "2022-05-20T08:24:13Z",
+    "content": "À partir du 1er juillet 2022, Ionity va changer ses tarifs pour facturer les clients au kilowattheure en France. C’est la fin d’une spécificité bien française, qui favorisait certains véhicules élect… [+4589 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Motor.ru"
+    },
+    "author": "Анастасия Мельник",
+    "title": "Покупателям Tesla Model X придётся ждать автомобиль еще два года",
+    "description": "Компания Tesla ещё в конце октября 2021 года объявила о начала поставок обновлённого электрического кроссовера Model X. С тех пор некоторые клиенты бренда действительно получили флагманскую модель, однако многие покупатели столкнулись с неоднократным переносо…",
+    "url": "https://motor.ru/news/teslatwoyears-20-05-2022.htm",
+    "urlToImage": "https://motor.ru/imgs/2022/05/20/08/5412802/1cb6a0e914df7010658480de96e8cb207fa70512.jpg",
+    "publishedAt": "2022-05-20T08:23:37Z",
+    "content": "Tesla , , Model X . , , 2021 , ."
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Android.com.pl"
+    },
+    "author": "Paweł Maretycz",
+    "title": "Śledził Elona Muska – teraz nastolatek śledzi samolot Zuckerberga",
+    "description": "Jack Sweeney to najbardziej nietypowy fan lotnictwa na świecie: pasjonują go samoloty znanych osób, takich jak Elon Musk. Teraz zaś rozszerzył on swoją listę zainteresowań i nastolatek śledzi samolot Zuckerberga. Jednak w tym przypadku konsekwencje mogą być… …",
+    "url": "https://android.com.pl/news/485701-sledzil-elona-muska-teraz-nastolatek-sledzi-samolot-zuckerberga/",
+    "urlToImage": "https://static.android.com.pl/uploads/2022/05/Nastolatek-sledzi-samolot-Zuckerberga.jpg",
+    "publishedAt": "2022-05-20T08:23:17Z",
+    "content": "Jack Sweeney to najbardziej nietypowy fan lotnictwa na wiecie: pasjonuj go samoloty znanych osób, takich jak Elon Musk. Teraz za rozszerzy on swoj list zainteresowa i nastolatek ledzi samolot Zuckerb… [+4208 chars]"
+    }
+    ]
+    }
